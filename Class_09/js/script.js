b74804d0941d5ca6416a0d17ee62c296522102c0
tatusmatrix <Tatjana.Solovjova@rtu.lv>
2022-06-13 20:42:58 +0300

    Class_09d

--- Class_09/js/script.js
+++ Class_09/js/script.js
@@ -1,5 +1,20 @@
+var text = "";
+console.log('  "  "  ');
+console.log(" '  ' ");
+console.log("text mainīgā vērtība pēc definēšanas:");
+console.log(text);
+text = text + "jauns teksta fragments";
+console.log(text);
+
+document.getElementById("paragrafs_ar_tekstu").innerHTML = text;
+
+text = text + " un vēl viens fragments";
+console.log(text);
+
 var x = 100;
 document.getElementById("dec").innerHTML = x;
+
+document.getElementById("paragrafs_ar_tekstu").innerHTML = text;
 
 /*
 >> un & (interesējošo bitu novietot vienmēr 0. pozīcijā un ar ((x>>?) & 1)
@@ -15,9 +30,9 @@ x >> 0 -> 60(dec) atbilst 0011 1100(bin)
                 0000 0001
                 0000 0000(bin) -> 0(dec)
 */
-console.log("0. bita attēlošana (pēc nobīdes pa 0 bitiem pa labi un 'reizināšanas' ar 1): "+zero_th_bit);
+//console.log("0. bita attēlošana (pēc nobīdes pa 0 bitiem pa labi un 'reizināšanas' ar 1): "+zero_th_bit);
 bit_sequence = zero_th_bit + bit_sequence;
-console.log("0. bits: " + bit_sequence);
+//console.log("0. bits: " + bit_sequence);
 
 zero_th_bit = (x >> 1) & 1;
 /*
@@ -26,9 +41,9 @@ zero_th_bit = (x >> 1) & 1;
                    0000 0001
                    0000 0000(bin) -> 0(dec)
 */
-console.log("1. bita attēlošana (pēc nobīdes pa 1 bitu pa labi un 'reizināšanas' ar 1): "+zero_th_bit);
+//console.log("1. bita attēlošana (pēc nobīdes pa 1 bitu pa labi un 'reizināšanas' ar 1): "+zero_th_bit);
 bit_sequence = zero_th_bit + bit_sequence;
-console.log("0. un 1. bits: " + bit_sequence);
+//console.log("0. un 1. bits: " + bit_sequence);
 
 /*
 ......... Jūsu algoritms dec skaitļa pārveidošanai par bināro skaitli
@@ -40,3 +55,17 @@ console.log("0. un 1. bits: " + bit_sequence);
 document.getElementById("bin_peec_algoritma").innerHTML = bit_sequence;
 
 document.getElementById("bin_peec_standarta_funkcijas").innerHTML = Number(x).toString(2);
+
+// gada procentu likme ir 0.01%  gadā 1 gadam, 0.02% gadā 2 gadiem, utt.
+// pirmais mainīgais - klienta V.U.
+// otrais mainīgais - noguldāma summa
+// ...
+/*
+Cien. ...klienta_vārts...,
+Mēs, banka, redzam, ka Jums kontā ir ... EUR liela naudas summa.
+Ja Jūs ieguldīsiet to mūsu banka uz vienu 1 gadu, tad pēc gada kā % Jūs saņemsiet ... EUR
+Ja Jūs ieguldīsiet to mūsu banka uz vienu 2 gadu, tad pēc gada kā % Jūs saņemsiet ... EUR
+Ja Jūs ieguldīsiet to mūsu banka uz vienu 3 gadu, tad pēc gada kā % Jūs saņemsiet ... EUR
+Ja Jūs ieguldīsiet to mūsu banka uz vienu 4 gadu, tad pēc gada kā % Jūs saņemsiet ... EUR
+Ja Jūs ieguldīsiet to mūsu banka uz vienu 5 gadu, tad pēc gada kā % Jūs saņemsiet ... EUR
+*/
