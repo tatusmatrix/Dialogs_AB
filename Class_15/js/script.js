4cbd4585316b95459c645692b75bf9d49dbd4b94
tatusmatrix <Tatjana.Solovjova@rtu.lv>
2022-07-06 20:43:15 +0300

    Class_15b

--- Class_15/js/script.js
+++ Class_15/js/script.js
@@ -11,8 +11,7 @@ function dialogs() {
   //var a = Number(prompt("Ievadiet a vērtību:")), b = Number(prompt("Ievadiet b vērtību:"));
   var a = 10,
     b = 100;
-  document.getElementById("summator_1").innerHTML =
-    a + " un " + b + " summa ir " + summator(a, b);
+  document.getElementById("summator_1").innerHTML = a + " un " + b + " summa ir " + summator(a, b);
 }
 
 dialogs();
@@ -47,31 +46,39 @@ let text = "fgnlsnjsnldna ,sk n nnvkjn  skjvnsdkj";
 let text = prompt("Cien. lietotāj, lūdzu, ievadi kādu teikumu:");
 2. skaitīt burtus vārdā, katra atsevišķā vārdā vienmēr sākot no 1
 */
-text = "a ab abc abcd abcde abcdef abcdefg";
+//text = "a ab abc abcd abcde abcdef abcdefg";
+text = prompt("Cien. lietotāj, lūdzu, ievadi kādu teikumu:");
 //text = text + " ";
 var burtu_skaitiitaajs_vaardaa = 0;
 var simbolu_skaitiitaajs_teikumaa = 0;
+var deriigu_vaardu_skaits = 0;
 N = 5;
 for (let x of text) {
   simbolu_skaitiitaajs_teikumaa++;
   if (x == " ") {
-    console.log(x + " tā ir atstarpe");
-    console.log("Iepriekšējā vārdā bija " + burtu_skaitiitaajs_vaardaa + " burti");
+    //console.log(x + " tā ir atstarpe");
+    //console.log("Iepriekšējā vārdā bija " + burtu_skaitiitaajs_vaardaa + " burti");
     if (burtu_skaitiitaajs_vaardaa >= N) {
-      console.log("Vēl vairāk, iepriekšējā vārdā bija " + N + " un vairāk burti");
+      //console.log("Vēl vairāk, iepriekšējā vārdā bija " + N + " un vairāk burti");
+      deriigu_vaardu_skaits++;
     }
     burtu_skaitiitaajs_vaardaa = 0;
   } else {
     burtu_skaitiitaajs_vaardaa++;
-    console.log(x + " tas ir " + burtu_skaitiitaajs_vaardaa + ". simbols vārdā");
+    //console.log(x + " tas ir " + burtu_skaitiitaajs_vaardaa + ". simbols vārdā");
 
     if (simbolu_skaitiitaajs_teikumaa == text.length) {
-      console.log("Iepriekšējā vārdā bija " + burtu_skaitiitaajs_vaardaa + " burti");
+      //console.log("Iepriekšējā vārdā bija " + burtu_skaitiitaajs_vaardaa + " burti");
       if (burtu_skaitiitaajs_vaardaa >= N) {
-        console.log("Vēl vairāk, iepriekšējā vārdā bija " + N + " un vairāk burti");
+        //console.log("Vēl vairāk, iepriekšējā vārdā bija " + N + " un vairāk burti");
+        deriigu_vaardu_skaits++;
       }
       burtu_skaitiitaajs_vaardaa = 0;
-      console.log("Un vēl vairāk šis bija pēdējais vārds teikumā");
+      //console.log("Un vēl vairāk šis bija pēdējais vārds teikumā");
     }
   }
 }
+
+document.getElementById("text_analysis").innerHTML = "Šajā teikumā:<br><b>" + text + 
+                                                    "</b><br>ir "+ deriigu_vaardu_skaits + " vārdi, kuru"+
+                                                    " garums ir >= " + N;
