fa1254e09ff14fe2718ffae1936b3cddf961f4f7
tatusmatrix <Tatjana.Solovjova@rtu.lv>
2022-07-25 19:24:23 +0300

    Class_20d

--- Class_20/js/script.js
+++ Class_20/js/script.js
@@ -1,36 +1,86 @@
+// https://www.w3schools.com/js/js_object_accessors.asp
+
 const person = {
-    firstName: "",
-    lastName: "Doe",
-    language: "",
-    get_language: function(){
-        return `${this.firstName} knows ${this.language}`;  
-    },
-    set_language: function(firstName,lang){
-        this.firstName = firstName;  
-        this.language = lang;  
-    },
-    get lang() {
-      return `${this.firstName} knows ${this.language}`;
-    },
-    set lang([firstName,lang]) {
-        this.firstName = firstName;
-        this.language = lang;
-      }
+  firstName: "",
+  lastName: "Doe",
+  language: "",
+  counter: 0,
+  get_language: function () {
+    return `${this.firstName} knows ${this.language}`;
+  },
+  set_language: function (firstName, lang) {
+    this.firstName = firstName;
+    this.language = lang;
+  },
+  get lang() {
+    return `${this.firstName} knows ${this.language}`;
+  },
+  set lang([firstName, lang]) {
+    this.firstName = firstName;
+    this.language = lang;
+  },
+  set multiply(value) {
+    //this.counter *= value;
+    this.counter = this.counter * value;
+  },
 };
 
+console.log(person);
+
+// Define setters and getters
+Object.defineProperty(person, "reset", {
+  get: function () {
+    this.counter = 0;
+  },
+});
+Object.defineProperty(person, "increment", {
+  get: function () {
+    this.counter++;
+  },
+});
+Object.defineProperty(person, "decrement", {
+  get: function () {
+    this.counter--;
+  },
+});
+Object.defineProperty(person, "add", {
+  set: function (value) {
+    this.counter += value;
+  },
+});
+Object.defineProperty(person, "subtract", {
+  set: function (value) {
+    this.counter -= value;
+  },
+});
+
+console.log(person);
+
+person.reset;
+console.log(person.counter);
+person.add = 5;
+console.log(person.counter);
+person.subtract = 1;
+console.log(person.counter);
+person.increment;
+console.log(person.counter);
+person.decrement;
+console.log(person.counter);
+person.multiply = 4;
+console.log(person.counter);
+
+/*
 // Set data from the object using data property:
 person.firstName = "Carl";
 person.language = "ee";
 document.getElementById("demo1").innerHTML = person.lang;
-
 // Set data to the object using method:
 person.set_language("Mike","lt");
 document.getElementById("demo2").innerHTML = person.lang;
-
 // Set data to the object using a setter:
 person.lang = ["Gregory","lv"];
 document.getElementById("demo3").innerHTML = person.lang;
-
+*/
 /*
   // Display data from the object using data property:
   document.getElementById("demo1").innerHTML = `${person.firstName} knows ${person.language}`;
