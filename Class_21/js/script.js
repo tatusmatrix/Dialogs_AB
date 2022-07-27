0e13669a3cd839474effd68d76ad16fb4de9255b
tatusmatrix <Tatjana.Solovjova@rtu.lv>
2022-07-27 20:38:38 +0300

    Class_21c

--- Class_21/js/script.js
+++ Class_21/js/script.js
@@ -1,3 +1,5 @@
+window.name = "original";
+
 let jsWindow = window.open(
   "http://127.0.0.1:5500/Dialogs_AB/Class_20/",
   //"https://www.w3schools.com/js/js_object_prototypes.asp",
@@ -5,20 +7,27 @@ let jsWindow = window.open(
   "height=600,width=800"
 );
 
+//jsWindow.window.console(jsWindow.opener);
+
 setTimeout(() => {
   window.open("http://127.0.0.1:5500/Dialogs_AB/Class_19/", "test");
   //window.open("https://www.w3schools.com/js/js_this.asp", "test");
 }, 3000);
 
 setTimeout(() => {
-  jsWindow.resizeTo(600, 300);// Will not work for third part resources because of CORS
+  //jsWindow.resizeTo(600, 300); // Will not work for third part resources because of CORS
+  jsWindow.resizeBy(-100,-100); // -"- (?)
+  //jsWindow.moveTo(deltaX,deltaY); // -"- (?)
+  jsWindow.moveBy(50,50); // -"- (?)
 }, 6000);
 
+/*
 setTimeout(() => {
-    jsWindow.close();
-  }, 9000);
-
-  setTimeout(() => {
-    window.open("http://127.0.0.1:5500/Dialogs_AB/Class_18/", "_blank");
-  }, 12000);
-  
+  jsWindow.close();
+}, 9000);
+*/
+/*
+setTimeout(() => {
+  window.open("http://127.0.0.1:5500/Dialogs_AB/Class_18/", "_blank");
+}, 12000);
+*/
