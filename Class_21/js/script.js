16be42bbb4d3a511b83f91a952731adc4532d861
tatusmatrix <Tatjana.Solovjova@rtu.lv>
2022-07-27 20:24:42 +0300

    Class_21b

--- Class_21/js/script.js
+++ Class_21/js/script.js
@@ -1,18 +1,24 @@
 let jsWindow = window.open(
   "http://127.0.0.1:5500/Dialogs_AB/Class_20/",
-  "about",
+  //"https://www.w3schools.com/js/js_object_prototypes.asp",
+  "test",
   "height=600,width=800"
 );
 
 setTimeout(() => {
-  window.open("http://127.0.0.1:5500/Dialogs_AB/Class_19/", "about");
+  window.open("http://127.0.0.1:5500/Dialogs_AB/Class_19/", "test");
+  //window.open("https://www.w3schools.com/js/js_this.asp", "test");
 }, 3000);
 
 setTimeout(() => {
-  jsWindow.resizeTo(600, 300);
+  jsWindow.resizeTo(600, 300);// Will not work for third part resources because of CORS
 }, 6000);
 
 setTimeout(() => {
     jsWindow.close();
   }, 9000);
+
+  setTimeout(() => {
+    window.open("http://127.0.0.1:5500/Dialogs_AB/Class_18/", "_blank");
+  }, 12000);
   
