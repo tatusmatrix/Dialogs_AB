d09b799a8f8c292c4793cf2ad37f57403b3e133f
tatusmatrix <Tatjana.Solovjova@rtu.lv>
2022-08-24 19:16:42 +0300

    Class_24a

---
+++ Class_25/js/script.js
@@ -0,0 +1,26 @@
+﻿// Piemēra - https://www.javascripttutorial.net/javascript-dom/document-object-model-in-javascript/ - HTML dokumenta analīze:
+/*
+<html>
+    <head>
+        <title>JavaScript DOM</title>
+    </head>
+    <body>
+        <p>Hello DOM!</p>
+    </body>
+</html>
+*/
+// <html><head><title>JavaScript DOM</title></head><body><p>Hello DOM!</p></body></html>
+
+const firstChildNode = document.documentElement.firstChild;
+if (firstChildNode.nodeType !== Node.COMMENT_NODE) {
+    console.warn("You should comment your code!");
+    console.log(firstChildNode.nodeType);
+    console.log(firstChildNode.nodeName);
+    console.log(firstChildNode.nodeValue);
+}
+else {
+    console.log("Good! :-) code is commented/described");
+    console.log(firstChildNode.nodeType);
+    console.log(firstChildNode.nodeName);
+    console.log(firstChildNode.nodeValue);
+}
