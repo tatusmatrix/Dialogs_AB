e375ad16bd3804ea80ed84874c752a0f962e4ae4
tatusmatrix <Tatjana.Solovjova@rtu.lv>
2022-08-24 21:03:02 +0300

    Class_25c

--- Class_25/js/script.js
+++ Class_25/js/script.js
@@ -55,12 +55,33 @@ console.log(fourthChildNode.nodeValue);
 // console.log(lastChildNode.nodeName);
 // console.log(lastChildNode.nodeValue);
 
-const p = document.getElementById('demo');
+// https://www.javascripttutorial.net/javascript-dom/javascript-getelementbyid/
+const p = document.getElementById('demo1');
 console.log(p);
 console.log("p.innerHTML: "+p.innerHTML);
-p.innerHTML = "Paragrāfa teksts (mainīts 1. reizi.)";
+p.innerHTML = "1. Paragrāfa teksts (mainīts 1. reizi.)";
 console.log("p.innerText: "+p.innerText);
-p.innerText = "Paragrāfa teksts (mainīts 2. reizi.)";
+p.innerText = "1. Paragrāfa teksts (mainīts 2. reizi.)";
 console.log("p.firstChild.nodeType: "+p.firstChild.nodeType);
 console.log("p.firstChild.textContent: "+p.firstChild.textContent);
-p.firstChild.textContent = "Paragrāfa teksts (mainīts 3. reizi.)";
+p.firstChild.textContent = "1. Paragrāfa teksts (mainīts 3. reizi.)";
+
+console.log(document.querySelectorAll('*[id]'));
+
+// https://www.javascripttutorial.net/javascript-dom/javascript-getelementsbyname/
+
+let btn = document.getElementById('btnRate');
+let output = document.getElementById('output');
+
+btn.addEventListener('click', () => {
+    let rates = document.getElementsByName('rate');
+    rates.forEach((rate) => {
+        //console.log(rate);
+        console.log(rate.value + ' ' + rate.checked);
+        if (rate.checked) {
+            output.innerText = `You selected: ${rate.value}`;
+        }
+    alert("Continue!");
+    });
+
+});
