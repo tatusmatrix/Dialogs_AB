9f9ea8925a9032466101e946ad68e5757fd1c914
tatusmatrix <Tatjana.Solovjova@rtu.lv>
2022-08-24 20:26:26 +0300

    Class_25b

--- Class_25/js/script.js
+++ Class_25/js/script.js
@@ -10,7 +10,7 @@
 </html>
 */
 // <html><head><title>JavaScript DOM</title></head><body><p>Hello DOM!</p></body></html>
-
+/*
 const firstChildNode = document.documentElement.firstChild;
 if (firstChildNode.nodeType !== Node.COMMENT_NODE) {
     console.warn("You should comment your code!");
@@ -24,3 +24,43 @@ else {
     console.log(firstChildNode.nodeName);
     console.log(firstChildNode.nodeValue);
 }
+*/
+/*
+const secondChildNode = firstChildNode.nextSibling;
+console.log("\n");
+console.log(secondChildNode.nodeType);
+console.log(secondChildNode.nodeName);
+console.log(secondChildNode.nodeValue);
+
+const thirdChildNode = secondChildNode.nextSibling;
+console.log("\n");
+console.log(thirdChildNode.nodeType);
+console.log(thirdChildNode.nodeName);
+console.log(thirdChildNode.nodeValue);
+
+const fourthChildNode = thirdChildNode.nextSibling;
+console.log("\n");
+console.log(fourthChildNode.nodeType);
+console.log(fourthChildNode.nodeName);
+console.log(fourthChildNode.nodeValue);
+*/
+// const fifthChildNode = fourthChildNode.nextSibling;
+// console.log(fifthChildNode.nodeType);
+// console.log(fifthChildNode.nodeName);
+// console.log(fifthChildNode.nodeValue);
+
+// const lastChildNode = document.documentElement.lastChild;
+// console.log("\n");
+// console.log(lastChildNode.nodeType);
+// console.log(lastChildNode.nodeName);
+// console.log(lastChildNode.nodeValue);
+
+const p = document.getElementById('demo');
+console.log(p);
+console.log("p.innerHTML: "+p.innerHTML);
+p.innerHTML = "Paragrāfa teksts (mainīts 1. reizi.)";
+console.log("p.innerText: "+p.innerText);
+p.innerText = "Paragrāfa teksts (mainīts 2. reizi.)";
+console.log("p.firstChild.nodeType: "+p.firstChild.nodeType);
+console.log("p.firstChild.textContent: "+p.firstChild.textContent);
+p.firstChild.textContent = "Paragrāfa teksts (mainīts 3. reizi.)";
