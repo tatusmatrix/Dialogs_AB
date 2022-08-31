0dc286d2256914c94e4b36c744c9f80bd112c725
tatusmatrix <Tatjana.Solovjova@rtu.lv>
2022-08-31 18:54:32 +0300

    Class_27a

---
+++ Class_27/js/script.js
@@ -0,0 +1,22 @@
+﻿// https://www.javascripttutorial.net/javascript-dom/javascript-events/
+
+let btn = document.querySelector('#btn');
+
+/*
+function display() {
+    alert('It was clicked!');
+}
+btn.addEventListener('click',display);
+*/
+
+/*
+btn.addEventListener('click',function() {
+    alert('It was clicked!');
+});
+*/
+
+btn.addEventListener('click', (event) => {
+    alert('It was clicked!');
+    console.log(event.type);
+});
+
