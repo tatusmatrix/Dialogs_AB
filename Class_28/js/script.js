266d6372a1aa946c74c27724275b569db032c3b6
tatusmatrix <Tatjana.Solovjova@rtu.lv>
2022-09-05 19:32:35 +0300

    Class_28b

--- Class_28/js/script.js
+++ Class_28/js/script.js
@@ -8,3 +8,30 @@ let firstChild = content.firstChild;
 console.log("...firstChild: ",firstChild);
 console.log("...firstChild.nodeType: ",firstChild.nodeType);
 console.log("...firstChild.nodeName: ",firstChild.nodeName);
+
+// https://www.javascripttutorial.net/javascript-dom/javascript-siblings/
+let getSiblings = function (e) {
+    // for collecting siblings
+    let siblings = []; 
+    // if no parent, return no sibling
+    if(!e.parentNode) {
+        return siblings;
+    }
+    // first child of the parent node
+    let sibling  = e.parentNode.firstChild;
+    
+    // collecting siblings
+    while (sibling) {
+        if (sibling.nodeType === 1 && sibling !== e) {
+            siblings.push(sibling);
+        }
+        sibling = sibling.nextSibling;
+    }
+    return siblings;
+};
+
+let siblings = getSiblings(document.querySelector('.current'));
+siblingText = siblings.map(e => e.innerHTML);
+console.log("siblings: ",siblings);
+console.log("children of parent: ",document.querySelector('.current').parentElement.children);
+console.log("siblings texts: ",siblingText);
