7dde8f2dea41f37a739247c0607972b84fab0985
tatusmatrix <Tatjana.Solovjova@rtu.lv>
2022-09-12 20:20:27 +0300

    Class_31b

--- Class_31/js/script.js
+++ Class_31/js/script.js
@@ -1,1 +1,17 @@
 ﻿// https://www.javascripttutorial.net/web-apis/javascript-cookies/
+
+expires = new Date('2022-09-12T20:23:00.000');
+
+document.cookie = `username=admin; path=/; expires=${expires.toGMTString()}`;
+
+let intervalID = setInterval(check_cookie, 1000);
+
+function check_cookie(){
+  var current = new Date();
+  console.log(current);
+  const str = document.cookie;
+  console.log(str);
+  if(str==''){
+    clearInterval(intervalID);
+  }
+}
