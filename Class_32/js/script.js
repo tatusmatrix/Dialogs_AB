b286a6ac505aaf85f50164b4e23fd62086e041f8
tatusmatrix <Tatjana.Solovjova@rtu.lv>
2022-09-14 19:03:10 +0300

    Class_32a

---
+++ Class_32/js/script.js
@@ -0,0 +1,53 @@
+﻿// https://www.javascripttutorial.net/web-apis/javascript-cookies/
+
+expires = new Date();
+//console.log(expires);
+expires.setSeconds(expires.getSeconds() + 30);
+//console.log(expires);
+
+var i = 1;
+document.cookie = `connection=${i}; path=/; expires=${expires.toGMTString()}`;
+console.log(document.cookie);
+
+// '_ga=GA1.1.1119525608.1663167970;
+//   _ga_Q3E3XZ7RL7=GS1.1.1663167970.1.1.1663167975.0.0.0;
+//   __gads=ID=a6487e8c79a9a1c1:T=1663167976:S=ALNI_Mb3EgK8SYnu51mYrWs2GwBhpSXesA'
+class Cookie {
+  static get(name) {
+    const cookieName = `${encodeURIComponent(name)}=`;
+    const cookie = document.cookie;
+    let value = null;
+
+    const startIndex = cookie.indexOf(cookieName);
+    if (startIndex > -1) {
+      let endIndex = cookie.indexOf(';', startIndex);
+      if (endIndex == -1) {
+        endIndex = cookie.length;
+      }
+      value = decodeURIComponent(
+        cookie.substring(startIndex + cookieName.length, endIndex)
+      );
+    }
+    console.log(value);
+    return value;
+  }
+
+  static set(name, value, expires, path, domain, secure) {
+    let cookieText = `${encodeURIComponent(name)}=${encodeURIComponent(value)}`;
+    if (expires instanceof Date) {
+      cookieText += `; expires=${expires.toGMTString()}`; // cookieText = cookieText + `; ${expires}`;
+    }
+
+    if (path) cookieText += `; path=${path}`;
+    if (domain) cookieText += `; domain=${domain}`;
+    if (secure) cookieText += `; secure`;
+
+    document.cookie = cookieText;
+  }
+
+  static remove(name, path, domain, secure) {
+    Cookie.set(name, '', new Date(0), path, domain, secure);
+  }
+}
+
+console.log("cookie with name connetion (value): " + Cookie.get('connection'));
